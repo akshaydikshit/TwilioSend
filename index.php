6492cc74613ac7af517809db30f8507e2f3567e9
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-04 00:09:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $client = new Client($account_sid, $auth_token);
 
 $client->messages->create(
     // the number you'd like to send the message to
-    '+16464645046',
+    '+19173490168',
     array(
         // A Twilio phone number you purchased at twilio.com/console
         'from' => '+16464551741',
